7eeff05ea9e1727898493732bce4b7e9fa152b0b
Joao Oliveira <jhcardosodeoliveira@gmail.com>
2026-03-08 16:41:30 +0000

    created first setup for route create_account

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,10 +1,31 @@
 from fastapi import APIRouter
+from app.models.database import User, db
+from sqlalchemy.orm import sessionmaker
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
-async def auth():
+async def home():
     """
     This route is the authentification route
     """
     return {"message": "You are in Auth", "authentification": False}
+
+@auth_router.post("/create_account")
+async def create_account(email: str, password: str, name: str):
+    """
+        problems:
+        1- session opened but if there is any error until the end, it will still be opened
+        2- no pattern for message, should be returning 200, 404 ....
+        3- no pattern for parameters, passing 3 variables, it will be cleaner if I pass a class instead 
+    """
+    Session = sessionmaker(bind=db)
+    session = Session()
+    user = session.query(User).filter(User.email == email).first()     #type:ignore
+    if user:
+        return{"message": "user already exists"}
+    else:
+        new_user = User(name, email, password)
+        session.add(new_user)
+        session.commit()
+        return{"message": "user created"}
